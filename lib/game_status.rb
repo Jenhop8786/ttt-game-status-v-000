52d274210a4c896e9852caddc7556e2f321c9b24
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-13 22:09:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+ WIN_COMBINATIONS = [ 
+ [0,1,2],
+ [3,4,5],
+ @@ -12,7 +10,6 @@ WIN_COMBINATIONS = [
+]
+def play(board)
+  puts "Welcome to Tic Tac Toe!"
+  while !over?(board)
+    turn(board)
+  end
+  @@ -44,8 +41,12 @@ def won?(board)
+  end
+end
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
+def draw?(board)
+  !won(board) && full?(board)
+end
+def over?(board)
+  @@ -59,7 +60,7 @@ def turn(board)
+    turn(board)
+  end
+  move(board, input, current_player(board))
+  display_board(board)
+end
+def position_taken(board, location)
+end
+  
